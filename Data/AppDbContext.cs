ca1b6e4e614f8b81d87fd0f7eb74c6b84d6733ec
elnurra <elnurra@code.edu.az>
2023-01-24 19:18:21 +0400

    Tunar Fiorella

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Fiorello.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
---
+++ Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+    }
+}
